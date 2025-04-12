7c296bebfa1fd08e7c0665db815655c88fbfa433
Hồ Đình Trí <162857265+trib2203591@users.noreply.github.com>
2025-04-12 21:25:57 +0700

    adjust commentdto

--- src/main/java/ctu/forum/dto/CommentDTO.java
+++ src/main/java/ctu/forum/dto/CommentDTO.java
@@ -1,7 +1,6 @@
 package ctu.forum.dto;
 
 
-import java.util.Date;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,8 +13,6 @@ public class CommentDTO {
     public String content;
     public String user_id;
     public String post_id;
-    public Date created_at;
-    public Date updated_at;
     public long vote_count;
 }
 
